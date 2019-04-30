77d3acc648fa778fb986d789a5ca14c5d061972d
Paulo Eduardo Groth <pauloegk@MacBook-Air-de-Paulo.local>
2019-04-29 22:00:39 -0400

    send project

---
+++ src/main/java/com/webflux/app/Spring/Reactive/WebFlux/SpringReactiveWebFluxApplication.java
@@ -0,0 +1,13 @@
+package com.webflux.app.Spring.Reactive.WebFlux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringReactiveWebFluxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringReactiveWebFluxApplication.class, args);
+	}
+
+}
